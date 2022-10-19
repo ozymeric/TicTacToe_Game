36c5a8f2cc859749d9cbedbb1aa734b8880cbfca
ozymeric <mail.ozanmeric@gmail.com>
2022-10-19 22:02:19 +0300

    prettified some code

--- javascript/game.js
+++ javascript/game.js
@@ -1,11 +1,18 @@
+//*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("enter player names first!!!");
-    return; // by adding return here, we stop the code here. If we try to start the game when the player names are not given by the players, first we get the alert, but then this code will show the gameboard in anycase. But if we put return here, if names are not given, code will show warning, and stop here, so the gameboard will not be seen.
   }
   activePlayerNameElement.textContent = players[0].name;
   activeGameArea.style.display = "block";
 }
+
+//*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
 
 function switchPlayer() {
   if (activePlayer === 0) {
@@ -16,14 +23,16 @@ function switchPlayer() {
   activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
+//*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
+
 function selectGameField(event) {
   if (event.target.tagName === "LI") {
     const selectedField = event.target;
 
     const selectedColumn = selectedField.dataset.col - 1;
-    // as we are performing a mathematical operation in this example, we don't need to add + to convert the string data into a number.
     const selectedRow = selectedField.dataset.row - 1;
-    // by decrementing 1 from these constants we get 0-2 which sits on index numbers for our 2 dimensional array.
 
     if (gameData[selectedRow][selectedColumn] > 0) {
       alert("Please select an empty field!")
@@ -31,173 +40,172 @@ function selectGameField(event) {
     }
 
     selectedField.textContent = players[activePlayer].symbol;
-    // now, only by changing the activePlayer value, we can change the player symbol on the gameboard.
     selectedField.classList.add("disabled");
-
     gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    // console.log(gameData);
-
 
     switchPlayer();
-
     checkForWinner();
-
   }
-  // Now that solves our problem
 }
+
+//*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
 
 function checkForWinner() {
   let clickedBox = gameData[event.target.dataset.row -1][event.target.dataset.col -1];
-
    // __________________________________TIE SCENARIO:
- 
-    if ( clickedBox > 0 &&
-        document.querySelector("#game-board li:nth-of-type(1)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(2)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(3)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(4)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(5)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(6)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(7)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(8)").textContent &&
-        document.querySelector("#game-board li:nth-of-type(9)").textContent
-      ){
+  if (clickedBox > 0 &&
+      document.querySelector("#game-board li:nth-of-type(1)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(2)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(3)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(4)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(5)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(6)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(7)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(8)").textContent &&
+      document.querySelector("#game-board li:nth-of-type(9)").textContent
+      ) {
         backdropElement.style.display = "block";
         gameOverElement.style.display = "flex";
         gameOverElementText.innerText = "It's a tie baby!!!";
-      }
+      } 
    // __________________________________PLAYER 1 SCENARIO:
-    
-   // ROWS LOGIC:
-    else if (clickedBox > 0 && clickedBox === 1) {
-    if (
-      gameData[0][0] === gameData[0][1] && 
-      gameData[0][1] === gameData[0][2] &&
-      gameData[0][2] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    } else if (
-      gameData[1][0] === gameData[1][1] && 
-      gameData[1][1] === gameData[1][2] &&
-      gameData[1][2] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    } else if (
-      gameData[2][0] === gameData[2][1] && 
-      gameData[2][1] === gameData[2][2] &&
-      gameData[2][2] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    }
+    // ROWS LOGIC:
+      else if (clickedBox > 0 && clickedBox === 1) {
+        if (
+          gameData[0][0] === gameData[0][1] && 
+          gameData[0][1] === gameData[0][2] &&
+          gameData[0][2] === 1
+          ) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+          } else if (
+          gameData[1][0] === gameData[1][1] && 
+          gameData[1][1] === gameData[1][2] &&
+          gameData[1][2] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        } else if (
+          gameData[2][0] === gameData[2][1] && 
+          gameData[2][1] === gameData[2][2] &&
+          gameData[2][2] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        }
     // COLUMNS LOGIC:
-    else if (
-      gameData[0][0] === gameData[1][0] && 
-      gameData[1][0] === gameData[2][0] &&
-      gameData[2][0] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    } else if (
-      gameData[0][1] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][1] &&
-      gameData[2][1] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    } else if (
-      gameData[0][2] === gameData[1][2] && 
-      gameData[1][2] === gameData[2][2] &&
-      gameData[2][2] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    }
+        else if (
+          gameData[0][0] === gameData[1][0] && 
+          gameData[1][0] === gameData[2][0] &&
+          gameData[2][0] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        } else if (
+          gameData[0][1] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][1] &&
+          gameData[2][1] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        } else if (
+          gameData[0][2] === gameData[1][2] && 
+          gameData[1][2] === gameData[2][2] &&
+          gameData[2][2] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        }
     // DIAGONAL LOGIC:
-    else if (
-      gameData[0][0] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][2] &&
-      gameData[2][2] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    } else if (
-      gameData[0][2] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][0] &&
-      gameData[2][0] === 1) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
-    }
-
+        else if (
+          gameData[0][0] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][2] &&
+          gameData[2][2] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        } else if (
+          gameData[0][2] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][0] &&
+          gameData[2][0] === 1) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerOneName.textContent + " WINS !!!";
+        }
+      }
   
    // __________________________________PLAYER 2 SCENARIO:
-  
-  } else if (clickedBox > 0 && clickedBox === 2) {
     // ROWS LOGIC:
-    if (
-      gameData[0][0] === gameData[0][1] && 
-      gameData[0][1] === gameData[0][2] &&
-      gameData[0][2] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-    } else if (
-      gameData[1][0] === gameData[1][1] && 
-      gameData[1][1] === gameData[1][2] &&
-      gameData[1][2] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+      else if (clickedBox > 0 && clickedBox === 2) {
+        if (
+          gameData[0][0] === gameData[0][1] && 
+          gameData[0][1] === gameData[0][2] &&
+          gameData[0][2] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+        } else if (
+          gameData[1][0] === gameData[1][1] && 
+          gameData[1][1] === gameData[1][2] &&
+          gameData[1][2] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
 
-    } else if (
-      gameData[2][0] === gameData[2][1] && 
-      gameData[2][1] === gameData[2][2] &&
-      gameData[2][2] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+        } else if (
+          gameData[2][0] === gameData[2][1] && 
+          gameData[2][1] === gameData[2][2] &&
+          gameData[2][2] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          }
+        // COLUMNS LOGIC:
+          else if (
+          gameData[0][0] === gameData[1][0] && 
+          gameData[1][0] === gameData[2][0] &&
+          gameData[2][0] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          } else if (
+          gameData[0][1] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][1] &&
+          gameData[2][1] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          } else if (
+          gameData[0][2] === gameData[1][2] && 
+          gameData[1][2] === gameData[2][2] &&
+          gameData[2][2] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          }
+        // DIAGONAL LOGIC:
+        else if (
+          gameData[0][0] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][2] &&
+          gameData[2][2] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          } else if (
+          gameData[0][2] === gameData[1][1] && 
+          gameData[1][1] === gameData[2][0] &&
+          gameData[2][0] === 2) {
+            backdropElement.style.display = "block";
+            gameOverElement.style.display = "flex";
+            gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
+          }
       }
-    // COLUMNS LOGIC:
-    else if (
-      gameData[0][0] === gameData[1][0] && 
-      gameData[1][0] === gameData[2][0] &&
-      gameData[2][0] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-      } else if (
-      gameData[0][1] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][1] &&
-      gameData[2][1] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-      } else if (
-      gameData[0][2] === gameData[1][2] && 
-      gameData[1][2] === gameData[2][2] &&
-      gameData[2][2] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-      }
-    // DIAGONAL LOGIC:
-    else if (
-      gameData[0][0] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][2] &&
-      gameData[2][2] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-      } else if (
-      gameData[0][2] === gameData[1][1] && 
-      gameData[1][1] === gameData[2][0] &&
-      gameData[2][0] === 2) {
-        backdropElement.style.display = "block";
-        gameOverElement.style.display = "flex";
-        gameOverElementText.innerText = playerTwoName.textContent + " WINS !!!";
-      }
-  }
 }
+
+//*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
+
